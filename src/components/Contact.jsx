323bbcac1a993c05b23dcb9669d21e781ed4b3e6
Bagas Permana <bagastelkomschool@gmail.com>
2025-06-16 20:26:55 +0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch('http://localhost:5001/api/contact', {
+    const response = await fetch('https://portofolio-backend.glitch.me/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
